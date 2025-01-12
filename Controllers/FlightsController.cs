d6daf254f768e6e85f83a0df17b5fe6a9a1325b1
Harun Işık <157045502+harunidev@users.noreply.github.com>
2025-01-12 21:08:23 +0300

    Add files via upload

---
+++ Controllers/FlightsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectFinal.Data;
+using ProjectFinal.Models;
+
+namespace ProjectFinal.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class FlightsController : Controller
+    {
+        private ProjectFinalContext db = new ProjectFinalContext();
+
+        // GET: Flights
+        public ActionResult Index()
+        {
+            return View(db.Flights.ToList());
+        }
+
+        // GET: Flights/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Flights flights = db.Flights.Find(id);
+            if (flights == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flights);
+        }
+
+        // GET: Flights/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Flights/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Plane_model,Cities,peoples,F_Class,Price,F_Image,Schedule,F_Available")] Flights flights)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Flights.Add(flights);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(flights);
+        }
+
+        // GET: Flights/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Flights flights = db.Flights.Find(id);
+            if (flights == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flights);
+        }
+
+        // POST: Flights/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Plane_model,Cities,peoples,F_Class,Price,F_Image,Schedule,F_Available")] Flights flights)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(flights).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(flights);
+        }
+
+        // GET: Flights/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Flights flights = db.Flights.Find(id);
+            if (flights == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flights);
+        }
+
+        // POST: Flights/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Flights flights = db.Flights.Find(id);
+            db.Flights.Remove(flights);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectFinal.Data;
+
+namespace ProjectFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ProjectFinalContext db = new ProjectFinalContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult Flight_List()
+        {
+            var flights = db.Flights.ToList();
+            return View("Flight_List", flights);
+        }
+
+
+
+        public ActionResult Flight_Info(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var flight = db.Flights.FirstOrDefault(f => f.ID == id);
+
+            if (flight == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flight);
+        }
+
+        [HttpPost]
+
+        public ActionResult ReserveFlight(int id)
+        {
+            // Fetch the flight by ID
+            var flight = db.Flights.FirstOrDefault(f => f.ID == id);
+
+            if (flight == null)
+            {
+                TempData["Message"] = "Flight not found.";
+                return RedirectToAction("Flight_Info", new { id });
+            }
+
+            if (!flight.F_Available)
+            {
+                TempData["Message"] = "Flight is already reserved.";
+                return RedirectToAction("Flight_Info", new { id });
+            }
+
+            // Reserve the flight
+            flight.F_Available = false;
+            db.SaveChanges();
+
+            TempData["Message"] = "Flight reserved successfully!";
+            return RedirectToAction("Flight_Info", new { id });
+        }
+        public ActionResult Flight_List_Data(string firm, string flightClass, int? people)
+        {
+            var flights = db.Flights.AsQueryable();
+
+            // Filter by Plane Model
+            if (!string.IsNullOrEmpty(firm))
+            {
+                flights = flights.Where(f => f.Plane_model == firm);
+            }
+
+            // Filter by Class (Business/Economy)
+            if (!string.IsNullOrEmpty(flightClass))
+            {
+                try
+                {
+                    var classType = (ProjectFinal.Models.Type)Enum.Parse(typeof(ProjectFinal.Models.Type), flightClass, true);
+                    flights = flights.Where(f => f.F_Class == classType);
+                }
+                catch
+                {
+                    // Return an empty list on invalid flightClass
+                    return PartialView("_FlightList", new List<ProjectFinal.Models.Flights>());
+                }
+            }
+
+            // Filter by number of people
+            if (people.HasValue)
+            {
+                flights = flights.Where(f => f.peoples >= people.Value);
+            }
+
+            // Convert to list and return partial view
+            return PartialView("_FlightListPartial", flights.ToList());
+        }
+    }
+}
